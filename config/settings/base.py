e787faef9e7360787a539e3f204967f10c607ab3
kz <zaramaluna1999@hotmail.com>
2020-06-16 18:24:35 -0500

    pagination added in the rides

--- config/settings/base.py
+++ config/settings/base.py
@@ -165,4 +165,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3,
 }
