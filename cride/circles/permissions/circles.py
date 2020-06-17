2c2b4e993f75bee5d5f476550bd48e6e30dd2dd4
kz <zaramaluna1999@hotmail.com>
2020-06-16 22:03:17 -0500

    permissions added

---
+++ cride/circles/permissions/circles.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+
+from cride.circles.models import Membership
+
+class IsCircleAdmin(BasePermission):
+    """Allow access only too circle admin"""
+
+    def has_object_permission(self, request, view, obj):
+        """Verify user have a membership in the obj"""
+        try:
+            Membership.objects.get(
+                user=request.user,
+                circle=obj,
+                is_admin=True,
+                is_active=True
+            )
+        except Membership.DoesNotExist:
+            return False
+        return True
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,14 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+
 from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
 
 from cride.circles.serializers import CircleModelSerializer
 
 from cride.circles.models import Circle, Membership
 
-class CircleViewSet(viewsets.ModelViewSet):
-    queryset = Circle.objects.all()
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+
     serializer_class = CircleModelSerializer
-    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """restrict list to public only"""
@@ -17,6 +22,13 @@ class CircleViewSet(viewsets.ModelViewSet):
             return queryset.filter(is_public=True)
         return queryset
     
+    def get_permissions(self):
+        """Assign permission based on action"""
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permission.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
+
     def perform_create(self, serializer):
         """Assign circle admin."""
         circle = serializer.save()
