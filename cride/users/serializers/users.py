217654a7233ea0ce6e8254084217c60b80a23511
kz <zaramaluna1999@hotmail.com>
2020-06-16 22:46:13 -0500

    user detail and permissions added

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -62,7 +62,7 @@ class UserSignUpSerializer(serializers.Serializer):
     def create(self, data):
         """Handle user and profile creation"""
         data.pop('password_confirmation')
-        user = User.objects.create_user(**data, is_verified=False)
+        user = User.objects.create_user(**data, is_verified=False, is_client=True)
         Profile.objects.create(user=user)
         self.send_confirmation_email(user)
         return user
--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -1,15 +1,34 @@
-from rest_framework import status, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from cride.users.permissions import IsAccountOwner
+
+from cride.circles.serializers import CircleModelSerializer
 from cride.users.serializers import UserLoginSerializer, UserModelSerializer, UserSignUpSerializer, AccountVerificationSerializer
 
-class UserViewSet(viewsets.GenericViewSet):
+from cride.users.models import User
+from cride.circles.models import Circle
+
+class UserViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """User view set
     Handle sign up, login and account verification
     """
     
+    queryset = User.objects.filter(is_active=True, is_client=True)
+    serializer_class = UserModelSerializer
+    lookup_field = 'username'
+
+    def get_permissions(self):
+        """Assign permissions based on action"""
+        if self.action in ['signup', 'login', 'verify']:
+            permissions = [AllowAny]
+        elif self.action == 'retrieve':
+            permissions = [IsAuthenticated, IsAccountOwner]
+        else: permissions = [IsAuthenticated]
+        return [permission() for permission in permissions]
+
     @action(detail=False, methods=['post'])
     def login(self, request):
         """User login"""
@@ -38,3 +57,17 @@ class UserViewSet(viewsets.GenericViewSet):
         serializer.save()
         data = {'message': 'Congratulation, now go share some rides!'}
         return Response(data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        """Add extra data to response"""
+        response = super(UserViewSet, self).retrieve(request, *args, **kwargs)
+        circles = Circle.objects.filter(
+            members=request.user,
+            membership__is_active=True
+        )
+        data = {
+            'user':response.data,
+            'circles':CircleModelSerializer(circles, many=True).data
+        }
+        response.data = data
+        return response
