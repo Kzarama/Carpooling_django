1e76d3760a6392a9439da2f9207d63e20bceaceb
kz <zaramaluna1999@hotmail.com>
2020-06-18 22:12:53 -0500

    delete a member of a circle

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -3,9 +3,12 @@ from rest_framework.generics import get_object_or_404
 
 from cride.circles.models import Circle, Membership
 
+from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.memberships import IsActiveCircleMember
+
 from cride.circles.serializers import MembershipModelSerializer
 
-class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class MembershipViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """Circle membership view set"""
 
     serializer_class = MembershipModelSerializer
@@ -19,9 +22,28 @@ class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         )
         return super(MembershipViewSet, self).dispatch(request, *args, **kwargs)
     
+    def get_permissions(self):
+        """Assign permissions based on action"""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        return [permission() for permission in permissions]
+
     def get_queryset(self):
         """Return circle members"""
         return Membership.objects.filter(
             circle=self.circle,
             is_active=True
         )
+
+    def get_object(self):
+        """Return the circle member by using the user's username"""
+        return get_object_or_404(
+            Membership,
+            user__username=self.kwargs['pk'],
+            circle=self.circle,
+            is_active=True
+        )
+
+    def perform_destroy(self, instance):
+        """Disable membership"""
+        instance.is_active=False
+        instance.save()
