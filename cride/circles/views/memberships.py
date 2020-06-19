c9fced56041de20eb4e1490d8432c5cce2e9e743
kz <zaramaluna1999@hotmail.com>
2020-06-18 21:52:28 -0500

    list members of a circle added

---
+++ cride/circles/views/memberships.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets, mixins
+from rest_framework.generics import get_object_or_404
+
+from cride.circles.models import Circle, Membership
+
+from cride.circles.serializers import MembershipModelSerializer
+
+class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Circle membership view set"""
+
+    serializer_class = MembershipModelSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        """Verify that the circle exists"""
+        slug_name = kwargs['slug_name']
+        self.circle = get_object_or_404(
+            Circle,
+            slug_name=slug_name
+        )
+        return super(MembershipViewSet, self).dispatch(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        """Return circle members"""
+        return Membership.objects.filter(
+            circle=self.circle,
+            is_active=True
+        )
