1831c4abdb1e49a3af69b020093587e06f7b3fcd
kz <zaramaluna1999@hotmail.com>
2020-06-21 17:58:22 -0500

    action to verify and unverify from the admin added

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -8,3 +8,17 @@ class CircleAdmin(admin.ModelAdmin):
     list_display = ('slug_name', 'name', 'is_public', 'verified', 'is_limited', 'members_limit')
     search_fields = ('slug_name', 'name')
     list_filter = ('is_public', 'verified', 'is_limited')
+
+    actions = ['make_verified', 'make_unverified']
+
+    def make_verified(self, request, queryset):
+        """Make circles verified"""
+        queryset.update(verified=True)
+    
+    make_verified.short_description = 'Make selected circles verified'
+
+    def make_unverified(self, request, queryset):
+        """Make circles unverified"""
+        queryset.update(verified=False)
+    
+    make_unverified.short_description = 'Make selected circles unverified'
