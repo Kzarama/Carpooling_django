401ef2c8e2a3b76e36dee328c8b9d3f07dbd8861
kz <zaramaluna1999@hotmail.com>
2020-06-21 15:01:06 -0500

    task celery changed

---
+++ cride/taskapp/tasks.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+from cride.users.models import User
+
+from celery.decorators import task
+
+import jwt
+import time
+from datetime import timedelta
+
+def gen_verification_token(user):
+    """Create JWT token that the user can use to verify its account"""
+    exp_date = timezone.now() + timedelta(days=3)
+    payload = {
+        'user': user.username,
+        'exp': int(exp_date.timestamp()),
+        'type': 'email_confirmation'
+    }
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    return token.decode()
+
+@task(name='send_confirmation_email', max_retries=3)
+def send_confirmation_email(user_pk):
+    """ Send account verification link to given user"""
+    for i in range(30):
+        time.sleep(1)
+        print("Sleeping", str(i+1))
+    user = User.objects.get(pk=user_pk)
+    verification_token = gen_verification_token(user)
+
+    subject = 'Welcome @{}! Verify your account to start using Compate Ride.'.format(user.username)
+    from_email = 'Comparte Ride <noreply@comparteride.com>'
+    text_content = 'This is an important message.'
+    content = render_to_string(
+        'emails/users/account_verification.html',
+        {'token': verification_token, 'user': user}
+    )
+    msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
+    msg.attach_alternative(content, "text/html")
+    msg.send()
--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,20 +1,18 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, password_validation
-from django.core.mail import EmailMultiAlternatives
 from django.core.validators import RegexValidator
-from django.template.loader import render_to_string
-from django.utils import timezone
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import UniqueValidator
+
+from cride.taskapp.tasks import send_confirmation_email
 
 from cride.users.serializers.profiles import ProfileModelSerializer
 
 from cride.users.models import User, Profile
 
 import jwt
-from datetime import timedelta
 
 class UserModelSerializer(serializers.ModelSerializer):
     """User model serializer"""
@@ -70,35 +68,8 @@ class UserSignUpSerializer(serializers.Serializer):
         data.pop('password_confirmation')
         user = User.objects.create_user(**data, is_verified=False, is_client=True)
         Profile.objects.create(user=user)
-        self.send_confirmation_email(user)
+        send_confirmation_email.delay(user_pk=user.pk)
         return user
-
-    def send_confirmation_email(self, user):
-        """ Send account verification link to given user"""
-        verification_token = self.gen_verification_token(user)
-
-        subject = 'Welcome @{}! Verify your account to start using Compate Ride.'.format(user.username)
-        from_email = 'Comparte Ride <noreply@comparteride.com>'
-        text_content = 'This is an important message.'
-        content = render_to_string(
-            'emails/users/account_verification.html',
-            {'token': verification_token, 'user': user}
-        )
-        msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
-        msg.attach_alternative(content, "text/html")
-        msg.send()
-
-
-    def gen_verification_token(self, user):
-        """Create JWT token that the user can use to verify its account"""
-        exp_date = timezone.now() + timedelta(days=3)
-        payload = {
-            'user': user.username,
-            'exp': int(exp_date.timestamp()),
-            'type': 'email_confirmation'
-        }
-        token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-        return token.decode()
 
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
